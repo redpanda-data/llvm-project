061313c405632a1dee0f263640e20b9e9301a55e
Noah Watkins <noah@redpanda.com>
2023-03-10 14:02:50 +0000

    [clang-tidy] add check for capturing lambda coroutines
    Signed-off-by: Noah Watkins <noah@redpanda.com>
    
    Reviewed By: ChuanqiXu
    
    Differential Revision: https://reviews.llvm.org/D137514
    
    (cherry picked from commit 54178fc6161a5856bea608dce0d726787a3b71c3)
    
    Conflicts:
          clang-tools-extra/clang-tidy/cppcoreguidelines/CMakeLists.txt
          clang-tools-extra/clang-tidy/cppcoreguidelines/CppCoreGuidelinesTidyModule.cpp
          clang-tools-extra/docs/clang-tidy/checks/list.rst

--- clang-tools-extra/clang-tidy/cppcoreguidelines/CppCoreGuidelinesTidyModule.cpp
+++ clang-tools-extra/clang-tidy/cppcoreguidelines/CppCoreGuidelinesTidyModule.cpp
@@ -14,6 +14,7 @@
 #include "../modernize/AvoidCArraysCheck.h"
 #include "../modernize/UseOverrideCheck.h"
 #include "../readability/MagicNumbersCheck.h"
+#include "AvoidCapturingLambdaCoroutinesCheck.h"
 #include "AvoidConstOrRefDataMembersCheck.h"
 #include "AvoidDoWhileCheck.h"
 #include "AvoidGotoCheck.h"
@@ -47,6 +48,8 @@ namespace cppcoreguidelines {
 class CppCoreGuidelinesModule : public ClangTidyModule {
 public:
   void addCheckFactories(ClangTidyCheckFactories &CheckFactories) override {
+    CheckFactories.registerCheck<AvoidCapturingLambdaCoroutinesCheck>(
+        "cppcoreguidelines-avoid-capturing-lambda-coroutines");
     CheckFactories.registerCheck<modernize::AvoidCArraysCheck>(
         "cppcoreguidelines-avoid-c-arrays");
     CheckFactories.registerCheck<AvoidConstOrRefDataMembersCheck>(
